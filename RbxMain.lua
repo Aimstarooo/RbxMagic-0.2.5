847a79840acdb775947d73f30424392a8529c392
PatelniaWtf <aimstarxd69@gmail.com>
2025-04-05 16:53:17 +0200

    Update RbxMain.lua
    [/] Fov Button Repaired

--- RbxMain.lua
+++ RbxMain.lua
@@ -475,5 +475,482 @@ TextBox.FocusLost:Connect(function(enterPressed)
 end)
 
 TextButton.MouseButton1Down:connect(function()
+-- Gui to Lua
+-- Version: 3.2
+
+-- Instances:
+
+local RbxMagic = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local XGui = Instance.new("TextButton")
+local RbxMagic_2 = Instance.new("TextLabel")
+local Version = Instance.new("TextLabel")
+local SET = Instance.new("TextButton")
+local SetCash = Instance.new("TextLabel")
+local DeleteGui = Instance.new("TextButton")
+local UICorner = Instance.new("UICorner")
+local ByAimstarForProjectLodzkie = Instance.new("TextLabel")
+local TextBox = Instance.new("TextBox")
+local FireServer = Instance.new("TextLabel")
+local WyjebSpeedCameras = Instance.new("TextButton")
+local AndPublicMain = Instance.new("TextLabel")
+local CoolDownDowstawText = Instance.new("TextLabel")
+local CoolDownDostaw = Instance.new("TextButton")
+local AutaWyjeb = Instance.new("TextLabel")
+local AutaWyjeb_2 = Instance.new("TextLabel")
+local AddAllCars = Instance.new("TextButton")
+local PoryRoku = Instance.new("TextLabel")
+local PoryRoku_2 = Instance.new("TextLabel")
+local TextButton = Instance.new("TextButton")
+local TextLabel = Instance.new("TextLabel")
+local OpenGui = Instance.new("TextButton")
+local UICorner_2 = Instance.new("UICorner")
+local TextLabel_2 = Instance.new("TextLabel")
+
+--Properties:
+
+RbxMagic.Name = "RbxMagic"
+RbxMagic.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+RbxMagic.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.Parent = RbxMagic
+Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.25818786, 0, 0.0334013291, 0)
+Frame.Size = UDim2.new(0, 618, 0, 680)
+Frame.Visible = false
+
+XGui.Name = "XGui"
+XGui.Parent = Frame
+XGui.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+XGui.BackgroundTransparency = 1.000
+XGui.BorderColor3 = Color3.fromRGB(255, 253, 253)
+XGui.BorderSizePixel = 0
+XGui.Position = UDim2.new(0.876565278, 0, 0, 0)
+XGui.Size = UDim2.new(0, 69, 0, 42)
+XGui.Font = Enum.Font.SourceSans
+XGui.Text = "X"
+XGui.TextColor3 = Color3.fromRGB(255, 0, 4)
+XGui.TextScaled = true
+XGui.TextSize = 14.000
+XGui.TextWrapped = true
+
+RbxMagic_2.Name = "RbxMagic"
+RbxMagic_2.Parent = Frame
+RbxMagic_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+RbxMagic_2.BackgroundTransparency = 1.000
+RbxMagic_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+RbxMagic_2.BorderSizePixel = 0
+RbxMagic_2.Size = UDim2.new(0, 200, 0, 50)
+RbxMagic_2.Font = Enum.Font.FredokaOne
+RbxMagic_2.Text = "RbxMagic"
+RbxMagic_2.TextColor3 = Color3.fromRGB(202, 79, 255)
+RbxMagic_2.TextScaled = true
+RbxMagic_2.TextSize = 14.000
+RbxMagic_2.TextWrapped = true
+
+Version.Name = "Version"
+Version.Parent = Frame
+Version.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Version.BackgroundTransparency = 1.000
+Version.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Version.BorderSizePixel = 0
+Version.Position = UDim2.new(0.391771019, 0, 0, 0)
+Version.Size = UDim2.new(0, 121, 0, 50)
+Version.Font = Enum.Font.FredokaOne
+Version.Text = "V 0.2.5"
+Version.TextColor3 = Color3.fromRGB(202, 79, 255)
+Version.TextScaled = true
+Version.TextSize = 14.000
+Version.TextWrapped = true
+
+SET.Name = "SET"
+SET.Parent = Frame
+SET.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+SET.BackgroundTransparency = 0.700
+SET.BorderColor3 = Color3.fromRGB(0, 0, 0)
+SET.BorderSizePixel = 0
+SET.Position = UDim2.new(0.11806798, 0, 0.312997341, 0)
+SET.Size = UDim2.new(0, 105, 0, 38)
+SET.Font = Enum.Font.SourceSans
+SET.Text = "ON"
+SET.TextColor3 = Color3.fromRGB(17, 0, 255)
+SET.TextScaled = true
+SET.TextSize = 14.000
+SET.TextWrapped = true
+
+SetCash.Name = "SetCash"
+SetCash.Parent = Frame
+SetCash.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+SetCash.BackgroundTransparency = 1.000
+SetCash.BorderColor3 = Color3.fromRGB(0, 0, 0)
+SetCash.BorderSizePixel = 0
+SetCash.Position = UDim2.new(0.103756711, 0, 0.180371359, 0)
+SetCash.Size = UDim2.new(0, 121, 0, 50)
+SetCash.Font = Enum.Font.FredokaOne
+SetCash.Text = "InfCash"
+SetCash.TextColor3 = Color3.fromRGB(0, 255, 21)
+SetCash.TextScaled = true
+SetCash.TextSize = 14.000
+SetCash.TextWrapped = true
+
+DeleteGui.Name = "DeleteGui"
+DeleteGui.Parent = Frame
+DeleteGui.BackgroundColor3 = Color3.fromRGB(255, 0, 4)
+DeleteGui.BorderColor3 = Color3.fromRGB(0, 0, 0)
+DeleteGui.BorderSizePixel = 0
+DeleteGui.Position = UDim2.new(0.696843088, 0, 0.0146988686, 0)
+DeleteGui.Size = UDim2.new(0, 100, 0, 37)
+DeleteGui.Font = Enum.Font.SourceSans
+DeleteGui.Text = "DESTROY GUI"
+DeleteGui.TextColor3 = Color3.fromRGB(17, 0, 255)
+DeleteGui.TextScaled = true
+DeleteGui.TextSize = 14.000
+DeleteGui.TextWrapped = true
+
+UICorner.Parent = DeleteGui
+
+ByAimstarForProjectLodzkie.Name = "By Aimstar | For ProjectLodzkie"
+ByAimstarForProjectLodzkie.Parent = Frame
+ByAimstarForProjectLodzkie.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ByAimstarForProjectLodzkie.BackgroundTransparency = 1.000
+ByAimstarForProjectLodzkie.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ByAimstarForProjectLodzkie.BorderSizePixel = 0
+ByAimstarForProjectLodzkie.Position = UDim2.new(0, 0, 0.0727609545, 0)
+ByAimstarForProjectLodzkie.Size = UDim2.new(0, 344, 0, 21)
+ByAimstarForProjectLodzkie.Font = Enum.Font.FredokaOne
+ByAimstarForProjectLodzkie.Text = "By Aimstar | For ProjectLodzkie"
+ByAimstarForProjectLodzkie.TextColor3 = Color3.fromRGB(0, 255, 21)
+ByAimstarForProjectLodzkie.TextScaled = true
+ByAimstarForProjectLodzkie.TextSize = 14.000
+ByAimstarForProjectLodzkie.TextWrapped = true
+
+TextBox.Parent = Frame
+TextBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextBox.BackgroundTransparency = 0.700
+TextBox.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextBox.BorderSizePixel = 0
+TextBox.Position = UDim2.new(0.357781768, 0, 0.312997341, 0)
+TextBox.Size = UDim2.new(0, 200, 0, 38)
+TextBox.Font = Enum.Font.SourceSans
+TextBox.Text = "Put fireserver name and click enter"
+TextBox.TextColor3 = Color3.fromRGB(17, 0, 255)
+TextBox.TextScaled = true
+TextBox.TextSize = 14.000
+TextBox.TextWrapped = true
+
+FireServer.Name = "FireServer"
+FireServer.Parent = Frame
+FireServer.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+FireServer.BackgroundTransparency = 1.000
+FireServer.BorderColor3 = Color3.fromRGB(0, 0, 0)
+FireServer.BorderSizePixel = 0
+FireServer.Position = UDim2.new(0.389982104, 0, 0.180371359, 0)
+FireServer.Size = UDim2.new(0, 164, 0, 49)
+FireServer.Font = Enum.Font.FredokaOne
+FireServer.Text = "FireServer"
+FireServer.TextColor3 = Color3.fromRGB(0, 255, 21)
+FireServer.TextScaled = true
+FireServer.TextSize = 14.000
+FireServer.TextWrapped = true
+
+WyjebSpeedCameras.Name = "WyjebSpeedCameras"
+WyjebSpeedCameras.Parent = Frame
+WyjebSpeedCameras.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+WyjebSpeedCameras.BackgroundTransparency = 0.750
+WyjebSpeedCameras.BorderColor3 = Color3.fromRGB(0, 0, 0)
+WyjebSpeedCameras.BorderSizePixel = 0
+WyjebSpeedCameras.Position = UDim2.new(0.35735333, 0, 0.497035354, 0)
+WyjebSpeedCameras.Size = UDim2.new(0, 105, 0, 38)
+WyjebSpeedCameras.Font = Enum.Font.SourceSans
+WyjebSpeedCameras.Text = "Delete"
+WyjebSpeedCameras.TextColor3 = Color3.fromRGB(17, 0, 255)
+WyjebSpeedCameras.TextScaled = true
+WyjebSpeedCameras.TextSize = 14.000
+WyjebSpeedCameras.TextStrokeTransparency = 0.000
+WyjebSpeedCameras.TextWrapped = true
+
+AndPublicMain.Name = "And PublicMain"
+AndPublicMain.Parent = Frame
+AndPublicMain.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+AndPublicMain.BackgroundTransparency = 1.000
+AndPublicMain.BorderColor3 = Color3.fromRGB(0, 0, 0)
+AndPublicMain.BorderSizePixel = 0
+AndPublicMain.Position = UDim2.new(0.0210355986, 0, 0.103643306, 0)
+AndPublicMain.Size = UDim2.new(0, 121, 0, 21)
+AndPublicMain.Font = Enum.Font.FredokaOne
+AndPublicMain.Text = "And public main"
+AndPublicMain.TextColor3 = Color3.fromRGB(0, 255, 21)
+AndPublicMain.TextScaled = true
+AndPublicMain.TextSize = 14.000
+AndPublicMain.TextWrapped = true
+
+CoolDownDowstawText.Name = "CoolDownDowstawText"
+CoolDownDowstawText.Parent = Frame
+CoolDownDowstawText.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+CoolDownDowstawText.BackgroundTransparency = 1.000
+CoolDownDowstawText.BorderColor3 = Color3.fromRGB(0, 0, 0)
+CoolDownDowstawText.BorderSizePixel = 0
+CoolDownDowstawText.Position = UDim2.new(0.767441869, 0, 0.180371359, 0)
+CoolDownDowstawText.Size = UDim2.new(0, 121, 0, 48)
+CoolDownDowstawText.Font = Enum.Font.FredokaOne
+CoolDownDowstawText.Text = "Off Delivery CoolDown"
+CoolDownDowstawText.TextColor3 = Color3.fromRGB(0, 255, 21)
+CoolDownDowstawText.TextScaled = true
+CoolDownDowstawText.TextSize = 14.000
+CoolDownDowstawText.TextWrapped = true
+
+CoolDownDostaw.Name = "CoolDownDostaw"
+CoolDownDostaw.Parent = Frame
+CoolDownDostaw.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+CoolDownDostaw.BackgroundTransparency = 0.700
+CoolDownDostaw.BorderColor3 = Color3.fromRGB(0, 0, 0)
+CoolDownDostaw.BorderSizePixel = 0
+CoolDownDostaw.Position = UDim2.new(0.781753123, 0, 0.312002659, 0)
+CoolDownDostaw.Size = UDim2.new(0, 105, 0, 38)
+CoolDownDostaw.Font = Enum.Font.SourceSans
+CoolDownDostaw.Text = "Delete"
+CoolDownDostaw.TextColor3 = Color3.fromRGB(17, 0, 255)
+CoolDownDostaw.TextScaled = true
+CoolDownDostaw.TextSize = 14.000
+CoolDownDostaw.TextWrapped = true
+
+AutaWyjeb.Name = "AutaWyjeb"
+AutaWyjeb.Parent = Frame
+AutaWyjeb.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+AutaWyjeb.BackgroundTransparency = 1.000
+AutaWyjeb.BorderColor3 = Color3.fromRGB(0, 0, 0)
+AutaWyjeb.BorderSizePixel = 0
+AutaWyjeb.Position = UDim2.new(0.103745148, 0, 0.410259008, 0)
+AutaWyjeb.Size = UDim2.new(0, 121, 0, 50)
+AutaWyjeb.Font = Enum.Font.FredokaOne
+AutaWyjeb.Text = "Add All Cars"
+AutaWyjeb.TextColor3 = Color3.fromRGB(0, 255, 21)
+AutaWyjeb.TextScaled = true
+AutaWyjeb.TextSize = 14.000
+AutaWyjeb.TextWrapped = true
+
+AutaWyjeb_2.Name = "AutaWyjeb"
+AutaWyjeb_2.Parent = Frame
+AutaWyjeb_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+AutaWyjeb_2.BackgroundTransparency = 1.000
+AutaWyjeb_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+AutaWyjeb_2.BorderSizePixel = 0
+AutaWyjeb_2.Position = UDim2.new(0.343227535, 0, 0.410258979, 0)
+AutaWyjeb_2.Size = UDim2.new(0, 121, 0, 50)
+AutaWyjeb_2.Font = Enum.Font.FredokaOne
+AutaWyjeb_2.Text = "Delete FotoRadars"
+AutaWyjeb_2.TextColor3 = Color3.fromRGB(0, 255, 21)
+AutaWyjeb_2.TextScaled = true
+AutaWyjeb_2.TextSize = 14.000
+AutaWyjeb_2.TextWrapped = true
+
+AddAllCars.Name = "AddAllCars"
+AddAllCars.Parent = Frame
+AddAllCars.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+AddAllCars.BackgroundTransparency = 0.700
+AddAllCars.BorderColor3 = Color3.fromRGB(0, 0, 0)
+AddAllCars.BorderSizePixel = 0
+AddAllCars.Position = UDim2.new(0.118067965, 0, 0.496820867, 0)
+AddAllCars.Size = UDim2.new(0, 105, 0, 38)
+AddAllCars.Font = Enum.Font.SourceSans
+AddAllCars.Text = "ADD"
+AddAllCars.TextColor3 = Color3.fromRGB(17, 0, 255)
+AddAllCars.TextScaled = true
+AddAllCars.TextSize = 14.000
+AddAllCars.TextWrapped = true
+
+PoryRoku.Name = "PoryRoku"
+PoryRoku.Parent = Frame
+PoryRoku.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+PoryRoku.BackgroundTransparency = 1.000
+PoryRoku.BorderColor3 = Color3.fromRGB(0, 0, 0)
+PoryRoku.BorderSizePixel = 0
+PoryRoku.Position = UDim2.new(-0.00143284479, 0, 0.949999988, 0)
+PoryRoku.Size = UDim2.new(0, 150, 0, 32)
+PoryRoku.Font = Enum.Font.FredokaOne
+PoryRoku.Text = "🌱spring🌱"
+PoryRoku.TextColor3 = Color3.fromRGB(255, 133, 11)
+PoryRoku.TextScaled = true
+PoryRoku.TextSize = 14.000
+PoryRoku.TextWrapped = true
+
+PoryRoku_2.Name = "PoryRoku"
+PoryRoku_2.Parent = Frame
+PoryRoku_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+PoryRoku_2.BackgroundTransparency = 1.000
+PoryRoku_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+PoryRoku_2.BorderSizePixel = 0
+PoryRoku_2.Position = UDim2.new(0.755848706, 0, 0.949999988, 0)
+PoryRoku_2.Size = UDim2.new(0, 150, 0, 32)
+PoryRoku_2.Font = Enum.Font.FredokaOne
+PoryRoku_2.Text = "🌱spring🌱"
+PoryRoku_2.TextColor3 = Color3.fromRGB(255, 133, 11)
+PoryRoku_2.TextScaled = true
+PoryRoku_2.TextSize = 14.000
+PoryRoku_2.TextWrapped = true
+
+TextButton.Parent = Frame
+TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.BackgroundTransparency = 0.700
+TextButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.BorderSizePixel = 0
+TextButton.Position = UDim2.new(0.584342122, 0, 0.495826185, 0)
+TextButton.Size = UDim2.new(0, 105, 0, 38)
+TextButton.Font = Enum.Font.SourceSans
+TextButton.Text = "Farm"
+TextButton.TextColor3 = Color3.fromRGB(17, 0, 255)
+TextButton.TextScaled = true
+TextButton.TextSize = 14.000
+TextButton.TextWrapped = true
+TextButton.Visible = false
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0.571382701, 0, 0.408788443, 0)
+TextLabel.Size = UDim2.new(0, 121, 0, 50)
+TextLabel.Font = Enum.Font.FredokaOne
+TextLabel.Text = "AutoFarm"
+TextLabel.TextColor3 = Color3.fromRGB(0, 255, 21)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14.000
+TextLabel.TextWrapped = true
+TextLabel.Visible = false
+
+OpenGui.Name = "OpenGui"
+OpenGui.Parent = RbxMagic
+OpenGui.BackgroundColor3 = Color3.fromRGB(143, 240, 255)
+OpenGui.BorderColor3 = Color3.fromRGB(0, 0, 0)
+OpenGui.BorderSizePixel = 0
+OpenGui.Position = UDim2.new(0.868578255, 0, 0.76758796, 0)
+OpenGui.Size = UDim2.new(0, 100, 0, 80)
+OpenGui.Visible = false
+OpenGui.Font = Enum.Font.SourceSans
+OpenGui.Text = "RBXMAGIC"
+OpenGui.TextColor3 = Color3.fromRGB(50, 255, 39)
+OpenGui.TextScaled = true
+OpenGui.TextSize = 14.000
+OpenGui.TextWrapped = true
+
+UICorner_2.Parent = OpenGui
+
+TextLabel_2.Parent = RbxMagic
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.BorderSizePixel = 0
+TextLabel_2.Position = UDim2.new(-0.000714314403, 0, 0.874673605, 0)
+TextLabel_2.Size = UDim2.new(0, 261, 0, 36)
+TextLabel_2.Font = Enum.Font.SourceSansBold
+TextLabel_2.Text = "Insert To Open Cheats ;)"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 0, 4)
+TextLabel_2.TextScaled = true
+TextLabel_2.TextSize = 14.000
+TextLabel_2.TextWrapped = true
+
+-- Scripts:
+
+local function BDWQRI_fake_script() -- Frame.InterfaceToggle 
+	local script = Instance.new('LocalScript', Frame)
+
+	local function onKeyPress(input)
+		if input.KeyCode == Enum.KeyCode.Insert then -- Change X to whatever key you want to press to open the gui.
+			if Gui.Visible == true then
+				Gui.Visible = false
+			else
+				Gui.Visible = true
+			end
+		end
+	end
+	
+	game:GetService("UserInputService").InputBegan:Connect(onKeyPress)
+end
+coroutine.wrap(BDWQRI_fake_script)()
+local function OCXN_fake_script() -- XGui.LocalScript 
+	local script = Instance.new('LocalScript', XGui)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		script.Parent.Parent.Parent.Frame.Visible = false
+		
+	end)
+	
+end
+coroutine.wrap(OCXN_fake_script)()
+local function VPVH_fake_script() -- OpenGui.LocalScript 
+	local script = Instance.new('LocalScript', OpenGui)
+
+	local UserInputService = game:GetService("UserInputService")
+	
+	function onKeyPress(inputObject, gameProcessedEvent)
+		if not gameProcessedEvent then
+			if inputObject.KeyCode == Enum.KeyCode.Insert then 
+				if script.Parent.Parent.Frame.Visible == false then
+					script.Parent.Parent.Frame.Visible = true
+				else
+					script.Parent.Parent.Frame.Visible = false
+				end
+			end
+		end
+	end
+	
+	UserInputService.InputBegan:connect(onKeyPress)
+end
+coroutine.wrap(VPVH_fake_script)()
+
+-- Scripts Lua 
+
+CoolDownDostaw.MouseButton1Down:connect(function()
+    while( true )
+    do
+        game.Players.LocalPlayer.CooldownEnd:Destroy()
+    end
+ end)
+
+AddAllCars.MouseButton1Down:connect(function()
+	for _, vehicle in
+    { "Renault Master", "Volvo S60R", "Kia Ceed SW I", "Bmw e60", "Toyota Prius", "Bmw e39", "Ford Transit", "Audi A4" }
+do
+    local args = {
+        [1] = "BuyCar",
+        [2] = vehicle,
+    }
+
+    game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("MainEvent"):FireServer(unpack(args))
+end
+end)
+
+WyjebSpeedCameras.MouseButton1Down:connect(function()
+    game.workspace.Other.SpeedCameras:Destroy()
+end)
+
+DeleteGui.MouseButton1Down:connect(function()
+    game.Players.LocalPlayer.PlayerGui.RbxMagic:Destroy()
+end)
+
+SET.MouseButton1Down:connect(function()
+	local args = {
+		[1] = "MoneyRem",
+		[2] = -math.huge,
+	}
+	
+	game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("MainEvent"):FireServer(unpack(args))
+end)
+
+local TextBox = script.Parent
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local messageEvent = ReplicatedStorage:WaitForChild("MessageEvent")
+
+TextBox.FocusLost:Connect(function(enterPressed)
+    if enterPressed and TextBox.Text ~= "" then
+        game:GetService(TextBox.Text)
+        TextBox.Text = ""
+	end
+end)
+
+TextButton.MouseButton1Down:connect(function()
 game.workspace.Camera.FieldOfView.Value = 120
 end)
